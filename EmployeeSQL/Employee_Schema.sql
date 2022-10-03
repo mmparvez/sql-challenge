3495d92da7de2c5ecbc1dea184c7e71ca2496e34
Parvez <alampmail@gmail.com>
2022-10-03 23:49:34 +1100

    Clean the schema file

---
+++ EmployeeSQL/Employee_Schema.sql
@@ -0,0 +1,57 @@
+﻿--Drop all previously created tables
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Physical
+
+CREATE TABLE departments (
+    dept_no VARCHAR(4)   NOT NULL,
+    dept_name VARCHAR(50)   NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(10)   NOT NULL,
+    title VARCHAR(30)   NOT NULL,
+    PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(10)   NOT NULL,
+    birth_date VARCHAR(10)   NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    sex VARCHAR(1)   NOT NULL,
+    hire_date VARCHAR(10)   NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY(emp_title_id) REFERENCES titles (title_id)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR(50)   NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(50)   NOT NULL,
+    emp_no INT   NOT NULL,
+    FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
